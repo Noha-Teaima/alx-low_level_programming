2e0de4cb8b489dd36dbd463e0d1c918c3b3355e6
Noha-Teaima <nohateaima@gmail.com>
2023-02-21 07:40:33 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * main - prints the string "Holberton" from a character array.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+README.md char c[] = "Holberton";
+README.md int i;
+
+README.md for (i = 0; i < 9; i++)
+README.md {
+README.md README.md _putchar(c[i]);
+README.md }
+README.md _putchar(n);
+README.md return (0);
+}
